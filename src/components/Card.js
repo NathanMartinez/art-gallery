1780b22102165d55b33f8c95ccfa5ec3fc506d2c
Nate Martinez <natemartinez@Nates-MBP.hsd1.nm.comcast.net>
2021-07-02 14:37:24 -0600

    Added basic layout

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import "../styles/card.css";
+
+export default function Card({ bgColor }) {
+  return (
+    <div className="card" style={{ background: bgColor }}>
+      <div className="card-body">
+        <h2 className="card-title">Art Title</h2>
+      </div>
+      <div className="description-section">
+        <h4>Description</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+          totam eum earum vero enim qui culpa commodi natus sit iste, aperiam
+          debitis omnis nisi, sapiente aliquam saepe voluptatibus. Rerum,
+          necessitatibus.
+        </p>
+      </div>
+    </div>
+  );
+}
